4b6062776f97913453d21acc57dfdf46af5413bc
min0936 <101571808+misnawk@users.noreply.github.com>
2024-05-24 13:02:04 +0900

    Add files via upload

---
+++ day19/data_type/index.js
@@ -0,0 +1,17 @@
+//data-type
+
+const a = "hello";
+// a.includes()
+const b = 123;
+const c = true;
+
+const d = ["커피", "빵", "아이스크림"];
+// d.map(()=>{})
+const e = document.createElement("div");
+// e.appendChild()
+const f = { name: "kim", age: 21 };
+// f.keys
+
+//type - casting
+//explicit : Number(),String(),Boolean(),Object....
+//implicit : +, !!, [], {}
