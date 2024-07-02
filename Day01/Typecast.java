36d0925d36e02507dccaabf45393593a62bdb000
min0936 <101571808+misnawk@users.noreply.github.com>
2024-07-02 10:08:03 +0900

    Add files via upload

---
+++ Day01/Typecast.java
@@ -0,0 +1,32 @@
+package Day01;
+
+public class Typecast {
+    public static void main(String[] args) {
+        int a = 20;
+
+
+                // 정수형 변수 선언 및 초기화
+                int number = 10;
+
+                // 실수형 변수 선언 및 초기화
+                double pi = 3.14;
+
+                // 문자형 변수 선언 및 초기화
+                char letter = 'A';
+
+                // 문자열형 변수 선언 및 초기화
+                String greeting = "Hello, World!";
+
+                // 불리언형 변수 선언 및 초기화
+                boolean isJavaFun = true;
+
+                // 변수 값 출력
+                System.out.println("Number: " + number);
+                System.out.println("Pi: " + pi);
+                System.out.println("Letter: " + letter);
+                System.out.println("Greeting: " + greeting);
+                System.out.println("Is Java Fun? " + isJavaFun);
+
+
+    }
+}
