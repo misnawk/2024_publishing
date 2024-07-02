79333e0b2b2f78b4e81af723928923837543202e
min0936 <101571808+misnawk@users.noreply.github.com>
2024-07-02 10:13:14 +0900

    Add files via upload

---
+++ JAVA_Day01/Hello.java
@@ -0,0 +1,12 @@
+package JAVA_Day01;
+
+public class Hello {
+    //main 이 있어야 시작 가능[entry point]
+    public static void main(String[] args) {
+        //js - new Date(), new Math()
+        //System.out.println == console.log()
+        System.out.println("Hello Java!");
+        System.out.println("강민석");
+        System.out.println("자바굳");
+    }
+}
